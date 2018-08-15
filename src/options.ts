52f3da54ebf7ff5b442b59b975dbbb2b06d70d19
Mark Sanders <marksanders@iheartmedia.com>
2018-08-15 14:09:21 -0500

    Add jsdoc to IRootAttributes interface

--- src/options.ts
+++ src/options.ts
@@ -613,6 +613,10 @@ export class WrapHandlers implements IWrapHandlers {
     }
 }
 
+/**
+ * Typed interface to use to provide attributes to the root
+ * element of an XML document.
+ */
 export interface IRootAttributes {
     [key: string]: string;
 }
